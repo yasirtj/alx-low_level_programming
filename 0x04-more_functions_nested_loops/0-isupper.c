2218036e4e6cf132e366625e5b850cf12076231f
yasirtj <yasirtj11@gmail.com>
2023-09-19 16:07:42 -0700

    check if uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,5 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
 /**
  * _isupper - check if c is upper
  *
@@ -23,7 +7,7 @@ int main(void)
  *
  * Return: 1 if its uppercase, 0 if not
 */
-int isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
