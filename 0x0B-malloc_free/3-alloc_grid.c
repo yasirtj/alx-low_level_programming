dc15caa1d4e8e183414bc0bef422a9be75ef8b75
yasirtj <yasirtj11@gmail.com>
2023-10-05 02:48:53 -0700

    update 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < width; i++)
 	{
-		array[i] = (int *)malloc(sizeof(int) * width);
+		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
