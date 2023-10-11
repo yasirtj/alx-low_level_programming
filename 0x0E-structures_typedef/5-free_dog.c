cf17df70c0c66cdb25327ef249ddea7825bf572f
yasirtj <yasirtj11@gmail.com>
2023-10-11 14:25:59 -0700

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - frees a dog
+ * @d: dog
+ * Return: Always 0 Success
+**/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d);
+}
