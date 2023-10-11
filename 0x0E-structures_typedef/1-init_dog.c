779b1f24f5167e421508e7e42a96e893d1366331
yasirtj <yasirtj11@gmail.com>
2023-10-11 11:59:08 -0700

    initialize struct dog once again

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,7 +17,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	d->name = malloc(sizeof(name) + 1);
 	d->owner = malloc(sizeof(owner) + 1);
-	if (d->name == NULL || d->owner == NULL)
+	if (d->name == NULL)
+		return;
+	if (d->owner == NULL)
 		return;
 	d->name = name;
 	d->age = age;
