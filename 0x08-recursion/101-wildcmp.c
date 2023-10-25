762f6376eaf377ebe3c0f8a3b2b084be565d4957
yasirtj <yasirtj11@gmail.com>
2023-10-25 08:38:12 -0700

    check if two strings are identical

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and check if they are identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if strings are identical, otherwise 0
+**/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
