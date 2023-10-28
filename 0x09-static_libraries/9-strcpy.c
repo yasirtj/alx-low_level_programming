7fe0839db8aa17ecaf19148e565f23b2e314361b
yasirtj <yasirtj11@gmail.com>
2023-10-28 13:56:48 -0700

    create static library

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointerd to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: copies the string pointed to by `src`
+ * including the terminating null byte (\0), to the buffer pointed to by `dest`
+ * Return: pointer to `dest`
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = -1;
+
+	do {
+		length++;
+		dest[length] = src[length];
+
+	} while (src[length] != '\0');
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,4 @@
+**0x09. C - Static libraries**
+* A static librar libmy.a containing functions
+* A script called create_static_lib.sh that creates a static library called liball.a
+from all the .c files that are in the current directory.
