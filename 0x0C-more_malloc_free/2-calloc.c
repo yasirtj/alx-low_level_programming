7ca3b927fbdc459f3301c3506d3dde35f6ec272e
yasirtj <yasirtj11@gmail.com>
2023-11-02 13:18:09 -0700

    simulate calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	memory_block = malloc(nmemb * sizeof(size));
 	if (memory_block != NULL)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < (nmemb * size); i++)
 		{
 			memory_block[i] = 0;
 		}
