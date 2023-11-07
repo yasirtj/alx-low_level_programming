467bfaa7d460c6f1f431592132cdc221d042d9f3
yasirtj <yasirtj11@gmail.com>
2023-11-07 14:28:48 -0800

    bugFix 5-free_dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,12 +6,13 @@
  * free_dog - frees a dog
  * @d: dog
  * Return: Always 0 Success
-**/
+ **/
 
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
 	free(d->name);
+	free(d->owner);
 	free(d);
 }
