c694621dfe7a289b95626c3d41573d688e51c832
yasirtj <yasirtj11@gmail.com>
2023-11-11 12:35:33 +0200

    bugFix print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -82,4 +82,14 @@ void print_all(const char * const format, ...)
 	}
 	va_end(args);
 	printf("\n");
-}	
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
+}
