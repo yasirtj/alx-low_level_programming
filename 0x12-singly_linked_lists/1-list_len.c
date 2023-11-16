dec05827fe7572c2e44aaa2ab44afc3bb4f77186
yasirtj <yasirtj11@gmail.com>
2023-11-16 22:50:59 +0200

    number of element of the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* list_len - returns number of elements of linked list
+* @h: linked list head
+* Return: number of elements of linked list
+**/
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	if (h == NULL)
+	{
+		printf("list is empty");
+	}
+	const list_t *ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr =ptr->next;
+	}
+	return (count);
+}
