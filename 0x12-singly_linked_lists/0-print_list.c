df81f17172533ee69376ffec7ad86d5e9f1cea0c
yasirtj <yasirtj11@gmail.com>
2023-11-16 12:37:33 -0800

    bugFix - betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,27 @@
-  #include "lists.h"
+#include "lists.h"
 
-  /**
-  * print_list - prints all elements of the list
-  * @h: the head pointer which points to the first node of the list
-  * Return: number of nodes
-  **/
-  size_t print_list(const list_t *h)
-  {
-    int count = 0;
-    const list_t *ptr = h;
+/**
+ * print_list - prints all elements of the list
+ * @h: the head pointer which points to the first node of the list
+ * Return: number of nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *ptr = h;
 
-      while (ptr != NULL)
-      {
-        count++;
-        if (ptr->str == NULL)
-          {
-            printf("[0] (nil)\n");
-          }
-          else
-          {
-            printf("[%d] %s\n", ptr->len, ptr->str);
-          }
-          ptr = ptr->next;
-      }
-    return (count);
-  }
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+	}
+	return (count);
+}
