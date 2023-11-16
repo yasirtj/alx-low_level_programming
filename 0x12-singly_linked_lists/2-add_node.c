0c885c6aa508b96ca8824da7e3605f96296c75e0
yasirtj <yasirtj11@gmail.com>
2023-11-16 14:33:47 -0800

    bugFix #1 add node at begining of node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,11 +9,14 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+
 	if (head == NULL || str == NULL)
 	{
 		return (NULL);
 	}
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
