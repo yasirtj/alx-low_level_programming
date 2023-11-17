d3d3e63b62d6ba1c4b8541cc3167cb063ae9ae1f
yasirtj <yasirtj11@gmail.com>
2023-11-17 21:28:48 +0200

    Free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_list - Frees a list
+* @head: First node pointer
+* Return: Nothing
+**/
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current->len);
+		free(current);
+		current = next;
+	}
+}
+
